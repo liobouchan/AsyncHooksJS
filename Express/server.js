6e7fb7caf8bbd242730be34a17607a3c23d0371f
Leonardo <liogothicvampire@gmail.com>
2022-03-17 16:18:00 -0600

    Setting up the Express Server
    Our server will accept an HTTP request on port 3000. It creates an Async Hook to track every request by calling the createRequestContext() in a middleware function - a function that has access to an HTTP's request and response objects. The server then sends a JSON response with the data captured by the Async Hook.

---
+++ Express/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const ah = require('./hooks');
+const app = express();
+const port = 3000;
+
+app.use((request, response, next) => {
+    const data = { headers: request.headers };
+    ah.createRequestContext(data);
+    next();
+});
+
+const requestHandler = (request, response, next) => {
+    const reqContext = ah.getRequestContext();
+    response.json(reqContext);
+    next()
+};
+
+app.get('/', requestHandler)
+
+app.listen(port, (err) => {
+    if (err) {
+        return console.error(err);
+    }
+    console.log(`server is listening on ${port}`);
+}); ñ
